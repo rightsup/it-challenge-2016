3bd5f98304d4c0f796ae436119f41672ef4d9c9f
Diego d'Ursel <diegodurs@gmail.com>
2016-10-24 15:07:20 +0200

    fix test class name

--- test/services/compute_distribution/test_invariant.rb
+++ test/services/compute_distribution/test_invariant.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'services/compute_distribution'
 
 module ComputeDistribution
-  class TestSimpleCase < Minitest::Test
+  class TestInvariant < Minitest::Test
     def test_owned_percentage_above_hundred
     end
   end
